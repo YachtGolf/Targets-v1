4f34f94439400972aa44e286d799178e2e542e66
YachtGolf <alan@offthedeck.com>
2026-02-25 10:39:30 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -10,16 +9,10 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      // Set base path for GitHub Pages deployment
-      base: '/Targets-v1/',
+      base: './', 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
       }
     };
 });
