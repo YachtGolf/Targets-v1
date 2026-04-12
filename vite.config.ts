f999501c76e3d33eec52ee6a32b2b25df13be3e1
Off the Deck <alan@offthedeck.com>
2026-04-12 18:46:55 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -9,6 +10,11 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './'),
+        },
+      },
       base: '/', 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
