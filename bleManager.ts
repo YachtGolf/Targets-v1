60c1a038512e4a00efd1adff72b2190e9b3d9ab0
Off the Deck <alan@offthedeck.com>
2026-04-22 13:02:12 +0100

    Add files via upload

--- bleManager.ts
+++ bleManager.ts
@@ -1,8 +1,8 @@
 import { audioService } from './audioService';
 
-export const BLE_SERVICE_UUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
-export const BLE_CHARACTERISTIC_UUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
-export const BLE_WRITE_CHARACTERISTIC_UUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+export const BLE_SERVICE_UUID = '6e400011-b5a3-f393-e0a9-e50e24dcca9e';
+export const BLE_CHARACTERISTIC_UUID = '6e400013-b5a3-f393-e0a9-e50e24dcca9e';
+export const BLE_WRITE_CHARACTERISTIC_UUID = '6e400012-b5a3-f393-e0a9-e50e24dcca9e';
 
 export class BLEManager extends EventTarget {
   public currentGameName: string = 'None';
